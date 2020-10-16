056af7afcd1e7f874e52d7aa44926851dd0849f4
Debbech Hichem (EXTERNAL) <Hichem.Debbech@hq.eapl.europcar.vwfs-ad>
2020-10-16 18:42:26 +0200

    init project structure

---
+++ src/main/java/com/debbech/geonames/batch/GeonamesSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.debbech.geonames.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeonamesSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeonamesSpringBatchApplication.class, args);
+	}
+
+}
